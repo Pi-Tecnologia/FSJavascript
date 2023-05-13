72c02bb4732239a10ef9f2d83d9e62604359c5cf
Pi-Tecnologia <108378354+Pi-Tecnologia@users.noreply.github.com>
2023-05-13 00:13:56 -0300

    definido arquivos estáticos e publicos e criada as primeiras rotas

--- Node.js-Express.js/index.js
+++ Node.js-Express.js/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-const port = process.env.port || 8080
 
+// definindo os arquivos estáticos (views)
+app.use(express.static(path.join(__dirname, 'views')))
+
+// definindo os arquivos públicos (css e javascript)
+app.use(express.static(path.join(__dirname, 'public')))
+
+// rotas
+app.get('/', (req, res) => {
+  res.render('views/index')
+})
+
+
+
+
+// 404 error (not found)
+app.use((req, res) => {
+  res.send('Página não encontrada')
+})
+
+// executando servidor
+const port = process.env.port || 8080
 app.listen(port, () => console.log(`O servidor está escutando na porta ${port}`))
